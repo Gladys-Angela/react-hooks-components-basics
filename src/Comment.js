ddb9ca29d950a63bee97308bcb665a4487cbd2b5
Gladys-Angela <gladysangelakamau@gmail.com>
2022-07-14 00:16:01 +0300

    component

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Comment() {
+  return   <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+}
+function Comment() {
   return <div>Naturally, I agree with this article.</div>;
 }
+
 
 export default Comment;
